856352c151c5033e6846a1d2d2661442262332cb
matthias <matthias@smartive.ch>
2022-12-06 18:32:42 +0100

    added index

--- src/components/button-round/button-round.tsx
+++ src/components/button-round/button-round.tsx
@@ -8,7 +8,7 @@ export type ButtonProps = {
 export const ButtonRound: FC<ButtonProps> = ({ onClick, children }) => (
   <button
     className={
-      'rounded-full bg-slate-600 w-12 h-12 p-4 hover:bg-slate-700 active:bg-slate-700 hover:outline active:outline hover:outline-xs active:outline-s hover:outline-slate-100 active:outline-slate-200'
+      'text-white rounded-full bg-slate-600 w-12 h-12 p-4 hover:bg-slate-700 active:bg-slate-700 hover:outline active:outline hover:outline-xs active:outline-s hover:outline-slate-100 active:outline-slate-200'
     }
     onClick={onClick}
   >

ed759e94e8bb05d944c22623f9f7117f0e76c98f
matthias <matthias@smartive.ch>
2022-12-13 17:16:08 +0100

    added build

--- src/components/button-round/button-round.tsx
+++ src/components/button-round/button-round.tsx
@@ -1,11 +1,11 @@
 import React, { FC, ReactNode } from 'react';
 
-export type ButtonProps = {
+type ButtonRoundProps = {
   onClick: () => void;
   children: ReactNode;
 };
 
-export const ButtonRound: FC<ButtonProps> = ({ onClick, children }) => (
+export const ButtonRound: FC<ButtonRoundProps> = ({ onClick, children }) => (
   <button
     className={
       'text-white rounded-full bg-slate-600 w-12 h-12 p-4 hover:bg-slate-700 active:bg-slate-700 hover:outline active:outline hover:outline-xs active:outline-s hover:outline-slate-100 active:outline-slate-200'
